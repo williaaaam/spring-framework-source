58d7348acd57947070a4af686e25729cfc4c4b11
zhouxinjian <zhouxinjian@xiaomi.com>
2022-05-02 20:15:48 +0800

    Spring Bean gc

--- spring-context-example/src/main/java/com/xjz/springframework/config/ohnyproxy/ProxyBeanMethodConfig.java
+++ spring-context-example/src/main/java/com/xjz/springframework/config/ohnyproxy/ProxyBeanMethodConfig.java
@@ -1,10 +1,14 @@
 package com.xjz.springframework.config.ohnyproxy;
 
+import com.xjz.springframework.domain.Bar;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 
-@Configuration(proxyBeanMethods = true) // 不会创建ProxyBeanMethodConfig CGLIB代理对象，Foo构造器被调用两次
+//@Configuration(proxyBeanMethods = true) // 不会创建ProxyBeanMethodConfig CGLIB代理对象，Foo构造器被调用两次
+@Configuration(proxyBeanMethods = false) // 不会创建ProxyBeanMethodConfig CGLIB代理对象，Foo构造器被调用两次
 public class ProxyBeanMethodConfig {
 
 	public ProxyBeanMethodConfig() {
@@ -15,11 +19,12 @@ public class ProxyBeanMethodConfig {
 	//	Object result = factoryMethod.invoke(factoryBean, args);
 	@Bean
 	public Bar bar() {
-		foo();
+		//foo();
 		return new Bar();
 	}
 
 	@Bean
+	@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 	public Foo foo() {
 		return new Foo();
 	}
@@ -36,13 +41,34 @@ public class ProxyBeanMethodConfig {
 		public void setFoo(Foo foo) {
 			this.foo = foo;
 		}
+
+		@Override
+		@SuppressWarnings("deprecation")
+		protected void finalize() throws Throwable {
+			try {
+				System.out.println("Invoke Bar finalize");
+			} finally {
+				super.finalize();
+			}
+		}
 	}
 
 	public class Foo {
 
+		@Override
+		@SuppressWarnings("deprecation")
+		protected void finalize() throws Throwable {
+			try {
+				System.out.println("Invoke Foo finalize");
+			} finally {
+				super.finalize();
+			}
+		}
+
 		public Foo() {
 			System.out.println("Foo构造器");
 		}
+
 	}
 
 }
